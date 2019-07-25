09bbc811dcdc3b6121894d35839316cdfb33f00d
Skyler Burgoyne <skyler.burgoyne13@yahoo.com>
2019-07-25 19:32:30 -0400

    Added Unity scripts

---
+++ Scripts/LaptopScript.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LaptopScript : MonoBehaviour {
+
+    public Image BackGroundImage;
+
+    public Text LaptopText;
+    public Color HighlightTextColor;
+    public Color NormalTextColor;
+
+    public Color NormalColor;
+    public Color HighlightColor;
+    public Color ClickColor;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnGazeEnter()
+    {
+        BackGroundImage.color = HighlightColor;
+        LaptopText.color = HighlightTextColor;
+    }
+
+    public void OnGazeExit()
+    {
+        BackGroundImage.color = NormalColor;
+        LaptopText.color = NormalTextColor;
+    }
+
+    public void OnClick()
+    {
+        ChangeCount.FileData.Append("Google " + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + "\n");
+
+        BackGroundImage.color = ClickColor;
+        SceneManager.LoadScene("Laptop");
+    }
+}
---
+++ Scripts/Office2Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Office2Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("Office2");
+    }
+}
